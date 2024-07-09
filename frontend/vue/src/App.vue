6613f341a2d15082731a0ae5d5577da609a5ba6d
Ravi Thinakkal <ravi.thinakkal@gmail.com>
2024-07-09 10:30:34 -0500

    added exists checks

--- frontend/vue/src/App.vue
+++ frontend/vue/src/App.vue
@@ -7,7 +7,7 @@
     >
     <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-    <v-toolbar-title>👋 Hello World 🌎</v-toolbar-title>
+    <v-toolbar-title>👋 Hello World! 🌎</v-toolbar-title>
   </v-app-bar>
 
     <v-main>
